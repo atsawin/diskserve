bf6f134eadf19306983a9b620a44bb316e60db3e
disksrv1 <root@server3.nakhon.net>
2012-03-25 14:42:27 +0700

    Change script directory

--- Controller/HomesController.php
+++ Controller/HomesController.php
@@ -5,18 +5,18 @@ class HomesController extends AppController {
   }
 
   public function run() {
-    shell_exec("sudo /www/disksrv2/app/script/run.sh");
+    shell_exec("sudo /www/disksrv1.nakhon.net/app/script/run.sh");
   }
 
   public function update() {
-    shell_exec("sudo /www/disksrv2/app/script/update.sh");
+    shell_exec("sudo /www/disksrv1.nakhon.net/app/script/update.sh");
   }
 
   public function clearCoW() {
-    shell_exec("sudo /www/disksrv2/app/script/clearCow.sh");
+    shell_exec("sudo /www/disksrv1.nakhon.net/app/script/clearCow.sh");
   }
 
   public function status() {
-    $this->set('status', shell_exec("sudo /www/disksrv2/app/script/status.sh"));
+    $this->set('status', shell_exec("sudo /www/disksrv1.nakhon.net/app/script/status.sh"));
   }
 }
